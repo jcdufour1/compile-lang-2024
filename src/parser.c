0a7d64fde818bd34b00aa7aede59dfbdb9ff9be8
Jonathan Dufour <jonathan@localhost.localdomain>
2024-08-15 19:12:37 -0500

    Some refactoring of parsing (started to use Tk_view)

--- src/parser.c
+++ src/parser.c
@@ -3,10 +3,11 @@
 #include "node.h"
 #include "nodes.h"
 #include "assert.h"
+#include "token_view.h"
 
-static inline void log_tokens(LOG_LEVEL log_level, const Token* tokens, size_t count) {
-    for (size_t idx = 0; idx < count; idx++) {
-        log(log_level, "    "TOKEN_FMT"\n", token_print(tokens[idx]));
+static inline void log_tokens(LOG_LEVEL log_level, Tk_view tokens) {
+    for (size_t idx = 0; idx < tokens.count; idx++) {
+        log(log_level, "    "TOKEN_FMT"\n", token_print(tokens.tokens[idx]));
     }
 }
 
@@ -19,9 +20,9 @@ typedef enum {
 } PARSE_STATE;
 
 // TODO: make this work when there are eg. nested (())
-static bool get_idx_matching_token(size_t* idx_matching, const Token* tokens, size_t count, TOKEN_TYPE type_to_match) {
-    for (size_t idx = 0; idx < count; idx++) {
-        if (tokens[idx].type == type_to_match) {
+static bool get_idx_matching_token(size_t* idx_matching, Tk_view tokens, TOKEN_TYPE type_to_match) {
+    for (size_t idx = 0; idx < tokens.count; idx++) {
+        if (tokens.tokens[idx].type == type_to_match) {
             *idx_matching = idx;;
             return true;
         }
@@ -30,32 +31,34 @@ static bool get_idx_matching_token(size_t* idx_matching, const Token* tokens, si
 }
 
 // this function will not consider nested ()
-static bool get_idx_token(size_t* idx_matching, const Token* tokens, size_t start, size_t count, TOKEN_TYPE type_to_match) {
-    for (size_t idx = start; idx < count; idx++) {
-        if (tokens[idx].type == type_to_match) {
-            *idx_matching = idx;;
+static bool get_idx_token(size_t* idx_matching, Tk_view tokens, size_t start, TOKEN_TYPE type_to_match) {
+    for (size_t idx = start; idx < tokens.count; idx++) {
+        if (tokens.tokens[idx].type == type_to_match) {
+            if (idx_matching) {
+                *idx_matching = idx;
+            }
             return true;
         }
     }
     return false;
 }
 
-static bool tokens_start_with_function_call(size_t* idx_semicolon, const Token* tokens, size_t count) {
-    if (count < 2) {
+static bool tokens_start_with_function_call(size_t* idx_semicolon, Tk_view tokens) {
+    if (tokens.count < 2) {
         return false;
     }
 
-    if (tokens[0].type != TOKEN_SYMBOL) {
+    if (tokens.tokens[0].type != TOKEN_SYMBOL) {
         return false;
     }
 
-    if (tokens[1].type != TOKEN_OPEN_PAR) {
+    if (tokens.tokens[1].type != TOKEN_OPEN_PAR) {
         return false;
     }
 
     size_t semicolon_pos;
-    get_idx_matching_token(&semicolon_pos, &tokens[0], count, TOKEN_SEMICOLON);
-    if (tokens[semicolon_pos - 1].type != TOKEN_CLOSE_PAR) {
+    get_idx_matching_token(&semicolon_pos, tokens, TOKEN_SEMICOLON);
+    if (tokens.tokens[semicolon_pos - 1].type != TOKEN_CLOSE_PAR) {
         return false;
     }
 
@@ -63,13 +66,13 @@ static bool tokens_start_with_function_call(size_t* idx_semicolon, const Token* 
     return true;
 }
 
-static Node_idx parse_rec(PARSE_STATE state, const Token* tokens, size_t count) {
+static Node_idx parse_rec(PARSE_STATE state, Tk_view tokens) {
     if (state == PARSE_FUN_PARAMS) {
-        if (tokens[0].type != TOKEN_OPEN_PAR) {
-            log(LOG_ERROR, "error: '(' expected\n");
-            exit(1);
+        if (tk_view_front(tokens).type == TOKEN_OPEN_PAR) {
+            log(LOG_ERROR, "error: '(' not expected\n");
+            abort();
         }
-        if (tokens[1].type != TOKEN_CLOSE_PAR) {
+        if (tokens.count > 0) {
             todo();
         }
         Node_idx parameters = node_new();
@@ -79,141 +82,144 @@ static Node_idx parse_rec(PARSE_STATE state, const Token* tokens, size_t count) 
 
     if (state == PARSE_FUN_ARGUMENTS) {
         size_t idx_semicolon;
-        if (get_idx_token(&idx_semicolon, tokens, 0, count, TOKEN_SEMICOLON)) {
+        if (get_idx_token(&idx_semicolon, tokens, 0, TOKEN_SEMICOLON)) {
             todo();
         }
+        bool end_after_this = false;
         Node_idx prev_node = NODE_IDX_NULL;
-        size_t start_curr_argument = 0; // inclusive
-        size_t end_curr_argument = 0; // inclusive
-        size_t idx_comma;
-        while (get_idx_token(&idx_comma, tokens, start_curr_argument, count, TOKEN_COMMA)) {
-            //log(LOG_TRACE, "token: "TOKEN_FMT"\n", token);
-            end_curr_argument = idx_comma - 1;
-            assert(end_curr_argument >= start_curr_argument);
-            size_t len_arg = end_curr_argument - start_curr_argument + 1;
-            Node_idx curr_node = parse_rec(PARSE_NORMAL, &tokens[start_curr_argument], len_arg);
+        while (1) {
+            Tk_view curr_arg_tokens;
+            if (get_idx_token(NULL, tokens, 0, TOKEN_COMMA)) {
+                curr_arg_tokens = Tk_view_chop_on_type_delim(&tokens, TOKEN_COMMA);
+                tk_view_chop_front(&tokens); // trim comma off tokens
+            } else {
+                curr_arg_tokens = tokens;
+                end_after_this = true;
+            }
+
+            Node_idx curr_node = parse_rec(PARSE_NORMAL, curr_arg_tokens);
             if (prev_node != NODE_IDX_NULL) {
                 nodes_set_next(prev_node, curr_node);
             }
-            start_curr_argument = idx_comma + 1;
+
             prev_node = curr_node;
+            if (end_after_this) {
+                return nodes_get_local_leftmost(curr_node);
+            }
         }
-        end_curr_argument = count - 1;
-        size_t len_arg = end_curr_argument - start_curr_argument + 1;
-        Node_idx curr_node = parse_rec(PARSE_NORMAL, &tokens[start_curr_argument], len_arg);
-        if (prev_node != NODE_IDX_NULL) {
-            nodes_set_next(prev_node, curr_node);
-        }
-        start_curr_argument = idx_comma + 1;
-        return nodes_get_local_leftmost(curr_node);
     }
 
     if (state == PARSE_FUN_RETURN_TYPES) {
-        if (count < 1) {
+        if (tokens.count < 1) {
             unreachable();
         }
-        if (count > 1) {
+        if (tokens.count > 1) {
             todo();
         }
         Node_idx return_types = node_new();
         nodes_at(return_types)->type = NODE_FUNCTION_RETURN_TYPES;
-        nodes_at(return_types)->lang_type = tokens[0].text;
+        nodes_at(return_types)->lang_type = tokens.tokens[0].text;
         return return_types;
     }
 
     if (state == PARSE_FUN_BODY) {
-        if (count < 2) {
+        if (tokens.count < 2) {
             unreachable();
         }
         Node_idx body = node_new();
         nodes_at(body)->type = NODE_FUNCTION_BODY;
-        nodes_set_right_child(body, parse_rec(PARSE_NORMAL, &tokens[1], count - 1));
+        assert(tokens.tokens[0].type == TOKEN_SYMBOL);
+        assert(tokens.tokens[1].type == TOKEN_OPEN_PAR);
+        size_t dummy;
+        assert(tokens_start_with_function_call(&dummy, tokens));
+        nodes_set_right_child(body, parse_rec(PARSE_NORMAL, tokens));
         return body;
     }
 
-    if (tokens[0].type == TOKEN_SYMBOL && 0 == str_view_cmp_cstr(tokens[0].text, "fn")) {
+    if (tk_view_front(tokens).type == TOKEN_SYMBOL && 0 == str_view_cmp_cstr(tk_view_front(tokens).text, "fn")) {
+        tk_view_chop_front(&tokens);
         Node_idx function = node_new();
         nodes_at(function)->type = NODE_FUNCTION_DEFINITION;
-        if (tokens[1].type == TOKEN_SYMBOL) {
-            nodes_at(function)->name = tokens[1].text;
+        if (tk_view_front(tokens).type == TOKEN_SYMBOL) {
+            nodes_at(function)->name = tk_view_chop_front(&tokens).tokens[0].text;
         } else {
             todo();
         }
+        Tk_view open_par = tk_view_chop_front(&tokens);
+        assert(open_par.tokens[0].type == TOKEN_OPEN_PAR);
 
         size_t parameters_len;
-        size_t parameters_start = 2;
-        if (tokens[3].type == TOKEN_CLOSE_PAR) {
+        if (tk_view_front(tk_view_chop_front(&tokens)).type == TOKEN_CLOSE_PAR) {
             parameters_len = 2;
         } else {
             todo();
         }
-        Node_idx parameters = parse_rec(PARSE_FUN_PARAMS, &tokens[parameters_start], parameters_len);
+        Tk_view parameters_tokens = tk_view_chop_count(&tokens, parameters_len - 2); // exclude ()
+        Node_idx parameters = parse_rec(PARSE_FUN_PARAMS, parameters_tokens);
         nodes_set_right_child(function, parameters);
 
         size_t return_types_len;
-        size_t return_types_start = parameters_start + parameters_len;
-        if (tokens[return_types_start].type == TOKEN_SYMBOL && 0 == str_view_cmp_cstr(tokens[return_types_start].text, "void")) {
+        if (tk_view_front(tokens).type == TOKEN_SYMBOL && 0 == str_view_cmp_cstr(tk_view_front(tokens).text, "void")) {
             return_types_len = 1;
         } else {
             todo();
         }
-        Node_idx return_types = parse_rec(PARSE_FUN_RETURN_TYPES, &tokens[return_types_start], return_types_len);
+        Tk_view return_type_tokens = tk_view_chop_count(&tokens, return_types_len);
+        Node_idx return_types = parse_rec(PARSE_FUN_RETURN_TYPES, return_type_tokens);
         nodes_set_next(parameters, return_types);
 
         size_t body_len;
-        size_t body_start = return_types_start + return_types_len;
-        assert(count > body_start);
-        if (tokens[body_start].type != TOKEN_OPEN_CURLY_BRACE) {
+        if (tk_view_front(tokens).type != TOKEN_OPEN_CURLY_BRACE) {
             unreachable();
         }
-        if (!get_idx_matching_token(&body_len, &tokens[body_start], count - body_start, TOKEN_CLOSE_CURLY_BRACE)) {
+        if (!get_idx_matching_token(&body_len, tokens, TOKEN_CLOSE_CURLY_BRACE)) {
             todo();
         }
-        Node_idx body = parse_rec(PARSE_FUN_BODY, &tokens[body_start], body_len);
+        tk_view_chop_front(&tokens);
+        Tk_view body_tokens = {.tokens = tokens.tokens, .count = body_len};
+        Node_idx body = parse_rec(PARSE_FUN_BODY, body_tokens);
         nodes_set_next(return_types, body);
 
         return function;
-    } 
+    }
 
     size_t semicolon_pos;
-    if (tokens_start_with_function_call(&semicolon_pos, tokens, count)) {
+    if (tokens_start_with_function_call(&semicolon_pos, tokens)) {
         Node_idx function_call = node_new();
         nodes_at(function_call)->type = NODE_FUNCTION_CALL;
-        nodes_at(function_call)->name = tokens[0].text;
-        log_tokens(LOG_TRACE, tokens, count);
-        size_t parameters_start = 2; // exclude outer ()
+        nodes_at(function_call)->name = tokens.tokens[0].text;
+        log_tokens(LOG_TRACE, tokens);
+        tk_view_chop_count(&tokens, 2); // exclude outer ()
         size_t parameters_end;
-        if (!get_idx_matching_token(&parameters_end, tokens, count, TOKEN_CLOSE_PAR)) {
+        if (!get_idx_matching_token(&parameters_end, tokens, TOKEN_CLOSE_PAR)) {
             todo();
         }
-        parameters_end--; // exclude outer ()
-        size_t count_to_pass_in = parameters_end - parameters_start + 1;
-        log(LOG_TRACE, "%zu %zu\n", parameters_start, parameters_end);
-        assert(count_to_pass_in < 1000);
-        nodes_set_right_child(function_call, parse_rec(PARSE_FUN_ARGUMENTS, &tokens[parameters_start], count_to_pass_in));
+        Tk_view parameters_tokens = tk_view_chop_count(&tokens, parameters_end); // exclude outer ()
+        nodes_set_right_child(function_call, parse_rec(PARSE_FUN_ARGUMENTS, parameters_tokens));
         return function_call;
     }
 
-    if (count == 1 && Token_is_literal(tokens[0])) {
+    if (tokens.count == 1 && Token_is_literal(tokens.tokens[0])) {
         Node_idx new_node = node_new();
         nodes_at(new_node)->type = NODE_LITERAL;
-        nodes_at(new_node)->name = tokens[0].text;
-        nodes_at(new_node)->literal_type = tokens[0].type;
+        nodes_at(new_node)->name = tokens.tokens[0].text;
+        nodes_at(new_node)->literal_type = tokens.tokens[0].type;
         return new_node;
     }
 
-    if (count < 1) {
+    if (tokens.count < 1) {
         return NODE_IDX_NULL;
     }
 
-    log(LOG_TRACE, "parse_rec other: "TOKEN_FMT"\n", token_print(tokens[0]));
-    log(LOG_TRACE, "parse_rec other: %zu\n", count);
-    log(LOG_TRACE, "cmp: %d\n", str_view_cmp_cstr(tokens[0].text, "fn"));
+    log(LOG_TRACE, "parse_rec other: "TOKEN_FMT"\n", token_print(tokens.tokens[0]));
+    log(LOG_TRACE, "parse_rec other: %zu\n", tokens.count);
+    log(LOG_TRACE, "cmp: %d\n", str_view_cmp_cstr(tokens.tokens[0].text, "fn"));
     unreachable();
 }
 
 void parse(const Tokens tokens) {
-    Node_idx root = parse_rec(PARSE_NORMAL, &tokens.buf[0], tokens.count);
+    Tk_view token_view = {.tokens = tokens.buf, .count = tokens.count};
+    Node_idx root = parse_rec(PARSE_NORMAL, token_view);
     log_tree(LOG_VERBOSE, root);
     todo();
 }
---
+++ src/token_view.h
@@ -0,0 +1,46 @@
+#ifndef TOKEN_VIEW_H
+#define TOKEN_VIEW_H
+
+#include <stddef.h>
+#include "token.h"
+
+typedef struct {
+    const Token* tokens;
+    size_t count;
+} Tk_view;
+
+static inline Token tk_view_at(Tk_view token_view, size_t idx) {
+    return token_view.tokens[idx];
+}
+static inline Token tk_view_front(Tk_view token_view) {
+    return tk_view_at(token_view, 0);
+}
+
+static inline Tk_view Tk_view_chop_on_type_delim(Tk_view* token_view, TOKEN_TYPE delim) {
+    Tk_view new_token_view;
+    for (size_t idx = 0; token_view->count > idx; idx++) {
+        if (token_view->tokens[idx].type == delim) {
+            new_token_view.tokens = token_view->tokens;
+            new_token_view.count = idx;
+            token_view->tokens += idx;
+            token_view->count -= idx;
+            return new_token_view;
+        }
+    }
+
+    // delim not found
+    todo();
+}
+
+static inline Tk_view tk_view_chop_count(Tk_view* token_view, size_t count) {
+    Tk_view new = {token_view->tokens, count};
+    token_view->tokens += count;
+    token_view->count -= count;
+    return new;
+}
+
+static inline Tk_view tk_view_chop_front(Tk_view* token_view) {
+    return tk_view_chop_count(token_view, 1);
+}
+
+#endif // TOKEN_VIEW_H
