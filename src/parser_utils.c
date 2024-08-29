8d07d3a67f00012ea49745bbfab4b9e1394cd67c
Jonathan Dufour <jonathan@localhost.localdomain>
2024-08-28 21:46:25 -0500

    wip: hello works with new system

--- src/parser_utils.c
+++ src/parser_utils.c
@@ -28,8 +28,29 @@ Node_id get_block_return_id(Node_id fun_call) {
 }
 
 Llvm_id get_prev_load_id(Node_id var_call) {
-    (void) var_call;
-    todo();
+    nodes_foreach_from_curr_rev(curr_node, var_call) {
+        if (nodes_at(curr_node)->type == NODE_LOAD && 0 == str_view_cmp(nodes_at(curr_node)->name, nodes_at(var_call)->name)) {
+            assert(nodes_at(curr_node)->llvm_id > 0);
+            return nodes_at(curr_node)->llvm_id;
+        }
+    }
+
+    Node_id parent_var_call = nodes_at(var_call)->parent;
+    switch (nodes_at(parent_var_call)->type) {
+        case NODE_FUNCTION_CALL:
+            break;
+        default:
+            unreachable();
+    }
+    
+    nodes_foreach_from_curr_rev(curr_node, parent_var_call) {
+        if (nodes_at(curr_node)->type == NODE_LOAD && 0 == str_view_cmp(nodes_at(curr_node)->name, nodes_at(var_call)->name)) {
+            assert(nodes_at(curr_node)->llvm_id > 0);
+            return nodes_at(curr_node)->llvm_id;
+        }
+    }
+
+    unreachable();
 }
 
 Llvm_id get_store_dest_id(Node_id var_call) {
