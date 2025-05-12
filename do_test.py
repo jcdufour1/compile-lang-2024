86fe355edd1f3cfaedefb044a1cdfe4659633445
Jonathan Dufour <jonathan@localhost.localdomain>
2025-05-12 12:30:39 -0500

    wip: do_test.py refactoring mostly done

--- do_test.py
+++ do_test.py
@@ -1,6 +1,7 @@
 import os, subprocess, sys, pathlib, difflib, multiprocessing
 from typing import Callable, Tuple, Optional
 from dataclasses import dataclass
+from enum import Enum
 
 class Changes:
     REMOVED = "\033[37;41m" #]
@@ -13,13 +14,14 @@ class StatusColors:
     BLUE = "\033[34;40m" #]
     TO_NORMAL = "\033[0m" #]
 
+class Action(Enum):
+    TEST = 1
+    UPDATE = 2
+
+
+@dataclass
 class FileItem:
-    def __init__(self, path: str, expected_success: bool):
-        self.path = path
-        self.expected_success = expected_success
-    path: str
-    expected_success: bool
-    expected_fail_str: list[str] | None # TODO: use Optional for consistency
+    path_base: str
 
 @dataclass
 class TestResult:
@@ -55,13 +57,12 @@ def get_files_to_test(files_to_test: list[str]) -> list[FileItem]:
     for possible_base in map(to_str, os.listdir(INPUTS_DIR)):
         possible_path = os.path.realpath(os.path.join(INPUTS_DIR, possible_base))
         if os.path.isfile(possible_path) and possible_path in files_to_test:
-            files.append(FileItem(possible_path, True))
+            files.append(FileItem(possible_base))
     return files
 
 def get_expected_output(file: FileItem) -> str:
-    expect_base, _ = os.path.splitext(os.path.basename(file.path))
-    expected = os.path.join(RESULTS_DIR, expect_base)
-    expected += ".txt"
+    expect_base: str = file.path_base
+    expected: str = os.path.join(RESULTS_DIR, expect_base)
     with open(expected, "r") as input:
         return input.read()
 
@@ -101,10 +102,10 @@ def compile_test(do_debug: bool, output_name: str, file: FileItem) -> TestResult
         assert(False and "not implemented")
 
     compile_cmd.append("compile")
-    compile_cmd.append(file.path)
+    compile_cmd.append(os.path.join(INPUTS_DIR, file.path_base))
     compile_cmd.append("--emit-llvm")
 
-    print_info("testing: " + file.path + " (" + debug_release_text + ")")
+    print_info("testing: " + os.path.join(INPUTS_DIR, file.path_base) + " (" + debug_release_text + ")")
     compile_out: subprocess.CompletedProcess[str] = subprocess.run(compile_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     if compile_out.returncode != 0:
         return TestResult(compile_out, None, None)
@@ -120,7 +121,7 @@ def compile_test(do_debug: bool, output_name: str, file: FileItem) -> TestResult
 
 
 # return true if test was successful
-def do_test(file: FileItem, do_debug: bool, expected_output: str, output_name: str) -> bool:
+def do_test(file: FileItem, do_debug: bool, expected_output: str, output_name: str, action: Action) -> bool:
     result: TestResult = compile_test(do_debug, output_name, file)
 
     debug_release_text: str
@@ -130,10 +131,19 @@ def do_test(file: FileItem, do_debug: bool, expected_output: str, output_name: s
         debug_release_text = "release"
 
     process_result: str = get_result_from_test_result(result)
+    if action == Action.UPDATE:
+        with open(os.path.join(RESULTS_DIR, file.path_base), "w") as newResult:
+            newResult.write(process_result)
+        return True
+    elif action == Action.TEST:
+        pass
+    else:
+        raise AssertionError("uncovered case")
+
     if process_result != expected_output:
         stdout_color: str = ""
         expected_color: str = ""
-        print_error("test fail:" + file.path + " (" + debug_release_text + ")")
+        print_error("test fail:" + os.path.join(INPUTS_DIR, file.path_base) + " (" + debug_release_text + ")")
         diff = difflib.SequenceMatcher(None, expected_output, process_result)
         for tag, expected_start, expected_end, stdout_start, stdout_end, in diff.get_opcodes():
             if tag == 'insert':
@@ -163,11 +173,11 @@ def do_test(file: FileItem, do_debug: bool, expected_output: str, output_name: s
         print(stdout_color)
         sys.exit(1)
 
-    print_success("testing: " + file.path + " (" + debug_release_text + ") success")
+    print_success("testing: " + os.path.join(INPUTS_DIR, file.path_base) + " (" + debug_release_text + ") success")
     print()
     return True
 
-def do_debug(files_to_test: list[str], count_threads: int, output_name: str) -> None:
+def do_debug(files_to_test: list[str], count_threads: int, output_name: str, action: Action) -> None:
     cmd = ["make", "-j", str(count_threads), "build"]
     print_info("compiling debug:")
     process = subprocess.run(cmd, env=dict(os.environ | {"DEBUG": "1"}))
@@ -177,16 +187,11 @@ def do_debug(files_to_test: list[str], count_threads: int, output_name: str) -> 
     print_success("compiling debug: done")
 
     for file in get_files_to_test(files_to_test):
-        expected_output: str
-        if file.expected_success:
-            expected_output = get_expected_output(file)
-        else:
-            expected_output = ""
-        do_test(file, do_debug=True, expected_output=expected_output, output_name=output_name)
+        do_test(file, do_debug=True, expected_output=get_expected_output(file), output_name=output_name, action=action)
     print_success("testing debug: done")
     print()
 
-def do_release(files_to_test: list[str], count_threads: int, output_name: str) -> None:
+def do_release(files_to_test: list[str], count_threads: int, output_name: str, action: Action) -> None:
     cmd = ["make", "-j", str(count_threads), "build"]
     print_info("compiling release:")
     process = subprocess.run(cmd, env=dict(os.environ | {"DEBUG": "0"}))
@@ -197,12 +202,7 @@ def do_release(files_to_test: list[str], count_threads: int, output_name: str) -
     print()
 
     for file in get_files_to_test(files_to_test):
-        expected_output: str
-        if file.expected_success:
-            expected_output = get_expected_output(file)
-        else:
-            expected_output = ""
-        do_test(file, do_debug=False, expected_output=expected_output, output_name=output_name)
+        do_test(file, do_debug=False, expected_output=get_expected_output(file), output_name=output_name, action=action)
     print_success("testing release: done")
     print()
 
@@ -216,12 +216,17 @@ def append_all_files(list_or_map: list | dict, callback: Callable):
 def add_to_map(map: dict, path: str):
     map[path] = 0
 
-def parse_args() -> Tuple[list[str], str]:
+def parse_args() -> Tuple[list[str], str, Action]:
+    action: Action = Action.TEST
     test_output = "" # TODO: be more consistant with test_output variable names
     to_include: dict[str, int] = {}
     has_found_flag = False
     for arg in sys.argv[1:]:
-        if arg.startswith("--output-file="):
+        if arg.startswith("--update"):
+            action = Action.UPDATE
+        elif arg.startswith("--test"):
+            action = Action.TEST
+        elif arg.startswith("--output-file="):
             test_output = arg.split("--output-file=")[1]
         elif arg.startswith("--exclude="):
             to_exclude_raw = arg[len("--exclude="):]
@@ -247,12 +252,13 @@ def parse_args() -> Tuple[list[str], str]:
     to_include_list: list[str] = []
     for path in to_include:
         to_include_list.append(path)
-    return to_include_list, test_output
+    return to_include_list, test_output, action
 
 def main() -> None:
     files_to_test: list[str]
-    output_file: str
-    files_to_test, test_output = parse_args()
+    test_output: str
+    action: Action
+    files_to_test, test_output, action = parse_args()
 
     count_threads: int
     try:
@@ -262,8 +268,8 @@ def main() -> None:
         print_warning(e, file=sys.stderr)
         count_threads = 2
 
-    do_debug(files_to_test, count_threads, test_output)
-    do_release(files_to_test, count_threads, test_output)
+    do_debug(files_to_test, count_threads, test_output, action)
+    do_release(files_to_test, count_threads, test_output, action)
     print_success("all tests passed")
 
 if __name__ == '__main__':
