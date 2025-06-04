50463238c5a56644160ff0a7e259af7c522d4ba6
jcdufour1 <134809669+jcdufour1@users.noreply.github.com>
2025-06-04 13:15:15 -0500

    actual .gitignore is implemented (#12)
    Co-authored-by: Jonathan Dufour <jonathan@localhost.localdomain>

--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-python3 do_test.py --output-file=test.c --exclude=examples/new_lang/safe_cast.own,/home/jonathan/src/compile-lang-2024/examples/new_lang/tuple.own,/home/jonathan/src/compile-lang-2024/tests/expected_failure_examples/tuple_invalid_subtype.own,/home/jonathan/src/compile-lang-2024/tests/expected_failure_examples/tuple_mismatched_counts.own
