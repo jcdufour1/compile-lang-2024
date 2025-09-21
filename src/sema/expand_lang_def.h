b137d51130d4875d2a651d064961a2c11388a459
Jonathan Dufour <134809669+jcdufour1@users.noreply.github.com>
2025-09-21 13:43:06 -0500

    Work on lang def (alias) (#34)
    error for undefined module alias in name_from_uname function
    
    Work on lang def (alias)
    
    ---------
    
    Co-authored-by: Jonathan Dufour <jonathan@localhost.localdomain>

--- src/sema/expand_lang_def.h
+++ src/sema/expand_lang_def.h
@@ -13,11 +13,11 @@ bool expand_def_def(Uast_def* def);
 
 bool expand_def_block(Uast_block* block);
 
-EXPAND_NAME_STATUS expand_def_uname(Uast_expr** new_expr, Uname* name, Pos dest_pos);
+EXPAND_NAME_STATUS expand_def_uname(Uast_expr** new_expr, Uname* name, Pos pos, Pos dest_pos);
 
 bool expand_def_expr_vec(Uast_expr_vec* exprs);
 
-bool expand_def_expr(Uast_expr* expr);
+bool expand_def_expr(Uast_expr** new_expr, Uast_expr* expr);
 
 bool expand_def_generic_param_vec(Uast_generic_param_vec* params);
 
@@ -26,5 +26,23 @@ bool expand_def_variable_def_vec(Uast_variable_def_vec* defs);
 EXPAND_NAME_STATUS expand_def_symbol(Uast_expr** new_expr, Uast_symbol* sym);
 
 bool expand_def_ulang_type(Ulang_type* lang_type, Pos dest_pos);
+
+bool expand_def_ulang_type_regular(
+    Ulang_type_regular* new_lang_type,
+    Ulang_type_regular lang_type,
+    Pos dest_pos
+);
+
+bool expand_def_function_def(Uast_function_def* def);
+
+bool expand_def_switch(Uast_switch* lang_switch);
+
+bool expand_def_stmt(Uast_stmt** new_stmt, Uast_stmt* stmt);
+
+bool expand_def_if_else_chain(Uast_if_else_chain* if_else);
+
+bool expand_def_operator(Uast_operator* oper);
+
+bool expand_def_defer(Uast_defer* lang_defer);
 
 #endif // EXPAND_LANG_DEF
