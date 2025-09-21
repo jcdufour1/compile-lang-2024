c710b5199d89305d27ee3f4d0f13f52ab424fd29
Jonathan Dufour <jonathan@localhost.localdomain>
2025-09-21 15:31:13 -0500

    wip: printf wrapper for slices has basic functionality

--- std/util.c
+++ std/util.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
+#include <stdarg.h>
 
 typedef struct {
     char* buf;
     size_t count;
 } Slice;
 
+#define BUF_SIZE 4096
+
 int own_printf(Slice format, ...) {
     va_list args;
-    va_start(args, n);  
+    va_start(args, format);  
 
-    vnprintf(buf, "%.*", format.count, format.buf, args);
+    static char format_buf[BUF_SIZE];
+    int status = sprintf(format_buf, "%.*s", (int)format.count, format.buf);
+    if (status < 0) {
+        goto end;
+    }
 
+    status = vprintf(format_buf, args);
+
+end:
     va_end(args);
+    return status;
 }
