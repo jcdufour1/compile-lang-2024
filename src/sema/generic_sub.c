379d5c0d11436bcddfeb4cf509489bb85d27a4fa
Jonathan Dufour <jonathan@localhost.localdomain>
2025-09-22 16:56:15 -0500

    str_to_int and str_from_int functions implemented in std

--- src/sema/generic_sub.c
+++ src/sema/generic_sub.c
@@ -12,6 +12,10 @@ void generic_sub_return(Uast_return* rtn, Name gen_param, Ulang_type gen_arg) {
     generic_sub_expr(rtn->child, gen_param, gen_arg);
 }
 
+void generic_sub_defer(Uast_defer* defer, Name gen_param, Ulang_type gen_arg) {
+    generic_sub_stmt(defer->child, gen_param, gen_arg);
+}
+
 void generic_sub_param(Uast_param* def, Name gen_param, Ulang_type gen_arg) {
     generic_sub_variable_def(def->base, gen_param, gen_arg);
     if (def->is_optional) {
@@ -191,7 +195,8 @@ void generic_sub_stmt(Uast_stmt* stmt, Name gen_param, Ulang_type gen_arg) {
             generic_sub_return(uast_return_unwrap(stmt), gen_param, gen_arg);
             return;
         case UAST_DEFER:
-            todo();
+            generic_sub_defer(uast_defer_unwrap(stmt), gen_param, gen_arg);
+            return;
         case UAST_YIELD:
             generic_sub_yield(uast_yield_unwrap(stmt), gen_param, gen_arg);
             return;
