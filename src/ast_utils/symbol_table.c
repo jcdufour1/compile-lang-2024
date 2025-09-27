4ac614db17cdb49aa0f69f9a375b5f6d4c0fef8c
Jonathan Dufour <jonathan@localhost.localdomain>
2025-09-26 19:13:08 -0500

    wip: fixed bug in usymbol_lookup relating to int literals

--- src/ast_utils/symbol_table.c
+++ src/ast_utils/symbol_table.c
@@ -310,37 +310,36 @@ bool usym_tbl_lookup(Uast_def** result, Name key) {
 }
 
 bool usymbol_lookup(Uast_def** result, Name key) {
-    if (strv_is_equal(key.mod_path, MOD_PATH_BUILTIN)) {
-        Name prim_key = key;
-        prim_key.scope_id = 0;
-        if (usym_tbl_lookup(result, prim_key)) {
-            return true;
-        }
-        if (lang_type_atom_is_signed(lang_type_atom_new(prim_key, 0))) {
-            int32_t bit_width = strv_to_int64_t(POS_BUILTIN, strv_slice(prim_key.base, 1, prim_key.base.count - 1));
-            Uast_primitive_def* def = uast_primitive_def_new(
-                POS_BUILTIN, lang_type_primitive_const_wrap(lang_type_signed_int_const_wrap(lang_type_signed_int_new((Pos) {0}, bit_width, 0)))
-            );
-            unwrap(usym_tbl_add(uast_primitive_def_wrap(def)));
-            *result = uast_primitive_def_wrap(def);
-            return true;
-        } else if (lang_type_atom_is_unsigned(lang_type_atom_new(prim_key, 0))) {
-            int32_t bit_width = strv_to_int64_t(POS_BUILTIN, strv_slice(prim_key.base, 1, prim_key.base.count - 1));
-            Uast_primitive_def* def = uast_primitive_def_new(
-                POS_BUILTIN, lang_type_primitive_const_wrap(lang_type_unsigned_int_const_wrap(lang_type_unsigned_int_new((Pos) {0}, bit_width, 0)))
-            );
-            unwrap(usym_tbl_add(uast_primitive_def_wrap(def)));
-            *result = uast_primitive_def_wrap(def);
-            return true;
-        } else if (lang_type_atom_is_float(lang_type_atom_new(prim_key, 0))) {
-            int32_t bit_width = strv_to_int64_t(POS_BUILTIN, strv_slice(prim_key.base, 1, prim_key.base.count - 1));
-            Uast_primitive_def* def = uast_primitive_def_new(
-                POS_BUILTIN, lang_type_primitive_const_wrap(lang_type_float_const_wrap(lang_type_float_new((Pos) {0}, bit_width, 0)))
-            );
-            unwrap(usym_tbl_add(uast_primitive_def_wrap(def)));
-            *result = uast_primitive_def_wrap(def);
-            return true;
-        }
+    Name prim_key = key;
+    prim_key.scope_id = 0;
+    prim_key.mod_path = MOD_PATH_BUILTIN;
+    if (usym_tbl_lookup(result, prim_key)) {
+        return true;
+    }
+    if (lang_type_atom_is_signed(lang_type_atom_new(prim_key, 0))) {
+        int32_t bit_width = strv_to_int64_t(POS_BUILTIN, strv_slice(prim_key.base, 1, prim_key.base.count - 1));
+        Uast_primitive_def* def = uast_primitive_def_new(
+            POS_BUILTIN, lang_type_primitive_const_wrap(lang_type_signed_int_const_wrap(lang_type_signed_int_new((Pos) {0}, bit_width, 0)))
+        );
+        unwrap(usym_tbl_add(uast_primitive_def_wrap(def)));
+        *result = uast_primitive_def_wrap(def);
+        return true;
+    } else if (lang_type_atom_is_unsigned(lang_type_atom_new(prim_key, 0))) {
+        int32_t bit_width = strv_to_int64_t(POS_BUILTIN, strv_slice(prim_key.base, 1, prim_key.base.count - 1));
+        Uast_primitive_def* def = uast_primitive_def_new(
+            POS_BUILTIN, lang_type_primitive_const_wrap(lang_type_unsigned_int_const_wrap(lang_type_unsigned_int_new((Pos) {0}, bit_width, 0)))
+        );
+        unwrap(usym_tbl_add(uast_primitive_def_wrap(def)));
+        *result = uast_primitive_def_wrap(def);
+        return true;
+    } else if (lang_type_atom_is_float(lang_type_atom_new(prim_key, 0))) {
+        int32_t bit_width = strv_to_int64_t(POS_BUILTIN, strv_slice(prim_key.base, 1, prim_key.base.count - 1));
+        Uast_primitive_def* def = uast_primitive_def_new(
+            POS_BUILTIN, lang_type_primitive_const_wrap(lang_type_float_const_wrap(lang_type_float_new((Pos) {0}, bit_width, 0)))
+        );
+        unwrap(usym_tbl_add(uast_primitive_def_wrap(def)));
+        *result = uast_primitive_def_wrap(def);
+        return true;
     }
 
     return generic_symbol_lookup(
