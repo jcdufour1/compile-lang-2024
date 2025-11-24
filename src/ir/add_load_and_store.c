a9b41fe4564aefcaf0592033fb0338b331ed51e5
Jonathan Dufour <jonathan@localhost.localdomain>
2025-11-23 22:51:20 -0600

    wip

--- src/ir/add_load_and_store.c
+++ src/ir/add_load_and_store.c
@@ -1313,6 +1313,9 @@ static Ir_name load_ptr_symbol(Ir_block* new_block, Tast_symbol* old_sym) {
     }
     unwrap(var_def);
     if (old_sym->base.lang_type.type != LANG_TYPE_VOID) {
+        log(LOG_DEBUG, FMT"\n", name_print(NAME_LOG, ir_name_to_name(ir_get_name(lang_alloca))));
+        log(LOG_DEBUG, FMT"\n", ir_lang_type_print(LANG_TYPE_MODE_LOG, lang_type_from_ir_name(ir_get_name(lang_alloca))));
+        log(LOG_DEBUG, FMT"\n", tast_symbol_print(old_sym));
         unwrap(ir_lang_type_get_pointer_depth(lang_type_from_ir_name(ir_get_name(lang_alloca))) > 0);
     }
 
