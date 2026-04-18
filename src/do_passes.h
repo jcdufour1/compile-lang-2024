79593ea8c3e83dae5db1194599e3420246583e73
Jonathan Dufour <jonathan@localhost.localdomain>
2026-04-18 15:18:39 -0500

    wip

--- src/do_passes.h
+++ src/do_passes.h
@@ -24,8 +24,6 @@ void construct_cfgs(void);
 
 void remove_void_assigns(void);
 
-void check_uninitialized(void);
-
 Ir_block* assign_ir_ids(Ir_block* root);
 
 void emit_llvm_from_tree(const Ir_block* root);
